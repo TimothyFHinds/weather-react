b2ef2287d13fc3d8ad9d1765f02842b0f3172559
Timothy Hinds <timothyfhinds@gmail.com>
2020-08-31 15:22:56 -0400

    https requests

---
+++ build/precache-manifest.185d847fff9d08a38942f8d6882cda06.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738f9ce46c5af67581cf7a9422784f0c",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "ccbfd8daec2cf0fd1730",
+    "url": "/weather-react/static/css/2.af4783e8.chunk.css"
+  },
+  {
+    "revision": "e7862eb147aa422f9df6",
+    "url": "/weather-react/static/css/main.c87be105.chunk.css"
+  },
+  {
+    "revision": "ccbfd8daec2cf0fd1730",
+    "url": "/weather-react/static/js/2.acc6154b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.acc6154b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7862eb147aa422f9df6",
+    "url": "/weather-react/static/js/main.90ae70a7.chunk.js"
+  },
+  {
+    "revision": "978dc9f2a9f9512ae584",
+    "url": "/weather-react/static/js/runtime-main.17cf43c5.js"
+  },
+  {
+    "revision": "7d1c5630e7a903d1d948c7b413b098f2",
+    "url": "/weather-react/static/media/background.7d1c5630.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weather-react/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
